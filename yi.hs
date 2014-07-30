e7056f6cb973a1660bf3b76ea703dd570e1e91c1
Andreas Farre <andreas.farre@gmail.com>
2014-07-30 10:17:16 +0200

    Add some key bindings.
    Setting with mark with C-Space seems not to work for some reason.
    Add delete word for C-KDel (here C-KBS doesn't work either, why?).

--- yi.hs
+++ yi.hs
@@ -1,5 +1,27 @@
+{-# Language FlexibleContexts, TypeOperators #-}
 import Yi
+import Yi.MiniBuffer
+import Yi.Keymap.Emacs (defKeymap, eKeymap, mkKeymap)
+import Yi.Keymap.Emacs.Utils (readUniversalArg)
+import Control.Monad
+import Data.Maybe
+import Prelude hiding ((.))
 
-config = defaultEmacsConfig
+config = defaultEmacsConfig { defaultKm = myKeymap }
 
 main = yi config
+
+myKeymap :: KeymapSet
+myKeymap = mkKeymap $ override Yi.Keymap.Emacs.defKeymap $ \proto _self ->
+   proto {
+           eKeymap = eKeymap proto ||>
+           -- Add a M-g binding for goto-line
+           (metaCh 'g' ?>>! (gotoLn . fromDoc :: Int ::: LineNumber -> BufferM Int)) ||>
+           ((ctrl $ spec KDel) ?>>! (deleteB unitWord Forward)) ||>
+           ((metaCh ' ') ?>>! placeMark)
+         }
+
+placeMark :: BufferM ()
+placeMark = do
+  putA highlightSelectionA True
+  pointB >>= setSelectionMarkPointB
